312160f366fedd2bcbaad30f0b6d2aff1787b45b
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-02-13 17:16:30 -0500

    Handling HTTP Errors

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,24 +1,43 @@
 import { useEffect } from 'react';
 import Places from './Places.jsx';
 import { useState } from 'react';
+import ErrorPage from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
 
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
 
     async function fetchPlaces() {
       setIsFetching(true);
-      const res = await fetch('http://localhost:3000/places')
-      const data = await res.json();
-      setAvailablePlaces(data.places);
+
+      try {
+        const res = await fetch('http://localhost:3000/places')
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error('Something went wrong! Failed to fetch places.');
+        }
+
+        setAvailablePlaces(data.places);
+
+      } catch (error) {
+        setError({ message: error.message || 'Failed to fetch places.' });
+        return;
+      }
       setIsFetching(false);
     }
-
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return (
+      <ErrorPage title="Error" message={error.message} />
+    )
+  }
 
 
   return (
